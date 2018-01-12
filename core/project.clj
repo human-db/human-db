4d9e8d72dab89afdd6f95055e17dd9e75035d08d
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-01-12 20:07:35 +0800

    Release 0.4.1

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject humandb.core "0.4.1-SNAPSHOT"
+(defproject humandb.core "0.4.1"
   :dependencies [[org.clojure/clojure "1.9.0"]]
 
   :profiles {:dev {:dependencies [[me.raynes/fs "1.4.6"]]}})
--- test/project.clj
+++ test/project.clj
@@ -1,5 +1,5 @@
-(defproject humandb.test "0.4.1-SNAPSHOT"
+(defproject humandb.test "0.4.1"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [humandb.core "0.4.1-SNAPSHOT"]
+                 [humandb.core "0.4.1"]
                  [humandb.processors.yaml "0.4.0-1"]
                  [humandb.persistors.github "0.4.0-3"]])
