743403998a01c6bbc212ffbdfda47c65bcda9f9d
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-01-12 20:05:41 +0800

    Update test dependency

--- test/project.clj
+++ test/project.clj
@@ -2,4 +2,4 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [humandb.core "0.4.1-SNAPSHOT"]
                  [humandb.processors.yaml "0.4.0-1"]
-                 [humandb.persistors.github "0.4.0-1"]])
+                 [humandb.persistors.github "0.4.0-3"]])
