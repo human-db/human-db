90da35d00fd225f353fb4f39b2297d47bdf44081
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-01-12 20:05:23 +0800

    Bump clojure version to 1.9.0

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-  :dependencies [[org.clojure/clojure "1.9.0-beta1"]]
 (defproject humandb.core "0.4.1-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.9.0"]]
 
   :profiles {:dev {:dependencies [[me.raynes/fs "1.4.6"]]}})
--- test/project.clj
+++ test/project.clj
@@ -1,5 +1,5 @@
-  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
 (defproject humandb.test "0.4.1-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [humandb.core "0.4.1-SNAPSHOT"]
                  [humandb.processors.yaml "0.4.0-1"]
                  [humandb.persistors.github "0.4.0-1"]])
