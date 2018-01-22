6092cb01bf0ff626c0cdfc0af6476d7654c484f6
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-01-22 11:53:28 +0800

    Release YAML

--- processor-yaml/project.clj
+++ processor-yaml/project.clj
@@ -1,4 +1,4 @@
-(defproject humandb.processors.yaml "0.4.0-1"
+(defproject humandb.processors.yaml "0.4.0-2"
   :dependencies [[org.clojure/clojure "1.9.0-beta1"]
                  [humandb.core "0.4.0"]
                  [io.forward/yaml "1.0.6"]])
