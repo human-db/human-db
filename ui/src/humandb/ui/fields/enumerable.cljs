d097c495ba95c09f6d5f7eaa9c1a0c7e0c57757f
Rafal Dittwald <rafal.dittwald@gmail.com>
2020-04-05 14:06:13 -0400

    Add list-options version of enum (vs just select)

--- ui/src/humandb/ui/fields/enumerable.cljs
+++ ui/src/humandb/ui/fields/enumerable.cljs
@@ -2,12 +2,33 @@
   (:require
     [humandb.ui.fields.core :refer [field]]))
 
-(defmethod field :enum
+(defn enum-select-one
   [{:keys [value disabled options on-change]}]
-  [:select {:value value
-            :disabled disabled
-            :on-change (fn [e]
-                         (on-change (.. e -target -value)))}
+  (let [lookup (reduce (fn [memo option]
+                         (assoc memo (str option) option))
+                       {} options)]
+    [:select {:value (str value)
+              :disabled disabled
+              :on-change (fn [e]
+                           (on-change (lookup (.. e -target -value))))}
+     (for [option options]
+       ^{:key (str option)}
+       [:option {:value option} (str option)])]))
+
+(defn enum-list-one
+  [{:keys [value disabled options on-change]}]
+  [:div
    (for [option options]
      ^{:key (str option)}
-     [:option {:value option} (str option)])])
+     [:label
+      [:input {:type "radio"
+               :checked (= value option)
+               :on-change (fn [e]
+                            (on-change option))}]
+      (str option)])])
+
+(defmethod field :enum
+  [{:keys [value disabled options on-change] :as config}]
+  (if (< (count options) 10)
+    [enum-list-one config]
+    [enum-select-one config]))
